31d791d4e105959dfb5a784bbfe75023976490d0
Helen Aquino <helen.aquino@shaw.ca>
2021-09-17 01:07:58 -0600

    write out code demoed in lab

---
+++ proxy_client.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import socket
+
+# Code from CMPUT 404 LAB 801 Lab 2 Demo, demoed by Zoe Riell
+
+# define address, buffer size
+HOST = "localhost"
+PORT = 8001
+BUFFER_SIZE = 1024
+
+payload = "GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n"
+
+def connect(addr):
+    # create socket. connect, and receive data
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(addr)
+        s.sendall(payload.encode())
+        s.shutdown(socket.SHUT_WR)
+        # shutdown() is different from close()
+
+        full_data = s.recv(BUFFER_SIZE)
+        print(full_data)
+
+    except Exception as e:
+        print(e)
+    
+    finally:
+        # REMEMBER TO CLOSE
+        s.close()
+
+def main():
+    connect((HOST, PORT))
+
+if __name__ == "__main__":
+    main()
